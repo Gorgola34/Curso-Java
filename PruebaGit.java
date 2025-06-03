eeadcd76fc43aeb343e8ed7141d2b56265e3d289
Salvador <salvadorabbott11@gmail.com>
2025-06-03 19:27:22 +0200

    Add Prueba

--- PruebaGit.java
+++ PruebaGit.java
@@ -3,7 +3,12 @@ public class PruebaGit {
     public static void main(String[] args) {
         
         String nombre="Salvador";
+        int x=22;
+        int y=45;
         System.out.println("bienvenido"+ " " +nombre);
+        System.out.println(x<y?"SI":"NO");
+
+
     }
     
 }
