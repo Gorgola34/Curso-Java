38840972b30d2eca2a7ea98d5f075ecb86487edc
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-03 12:00:50 +0200

    Intial commit

---
+++ HolaMundo/HelloWorld.java
@@ -0,0 +1,11 @@
+package HolaMundo;
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        String grettings="Hola Mundo";
+        System.out.println("-------");
+        System.out.println(grettings);
+    }
+    
+}
