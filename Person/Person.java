082856a8b7855e61f64d913aa8da723a53c38b83
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-04 10:58:23 +0200

    Add

--- Person/Person.java
+++ Person/Person.java
@@ -1,10 +1,29 @@
 package Person;
 
 public class Person {
+//Tipos de numeros enteros(datos primitivos)
+    //1 byte-byte
+    //2 byte-short
+    //4 byte-int
+    //8 byte- long
+//Tipos numero decimales primitivos
+    // 4 byte- float
+    // 8 byte-double
 
     public static void main(String[] args) {
-        String person="Person";
-        System.out.println("Hola"+" "+ person);
+        String person="Salvador";
+        System.out.println("Hola soy una persona llamada"+" "+ person);
+        byte age; //Declaracion de variable
+        age=22;  //Inicializacion o asignacion de variable
+        short birthYear=1990;
+        double height=1.78;
+        int cityPopulation=4_000_000;
+        long countryPopulation=40_000_000;
+        float decimal=23.6f;
+        double weight=65.74;
+        boolean haspPet=true;
+        char type='A';
+        String name="Juan";
     }
     
 }
