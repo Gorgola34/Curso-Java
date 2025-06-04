ddd16f23c5fcafd52e65485928b9f8c7ee94795a
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-04 12:24:08 +0200

    Exercise:String

---
+++ Person/Pipo.java
@@ -0,0 +1,25 @@
+package Person;
+
+public class Pipo {
+
+    public static void main(String[] args) {
+        int age=23;
+        double height=1.84;
+        String name="Pipo";
+        double weight=64.6;
+        boolean hasHouse=true;
+        Short birthYear=2010;
+        char letter='M';
+        System.out.println("""
+                Nombre: %s
+                Edad %s
+                Peso: %s
+                Altura: %s
+                ¿Tiene una casa?: %s
+                Año de nacimiento: %s
+                Letra favorita %s
+                """.formatted(name,age,weight,height,hasHouse,birthYear,letter));
+
+    }
+    
+}
