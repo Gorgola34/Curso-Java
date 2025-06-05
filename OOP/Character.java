4ed7027d944113511a730aa3a843fd0d40cb3a86
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-05 13:44:35 +0200

    Add POO

---
+++ OOP/Character.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class Character {
+    String name;
+    int hp;
+    char type;
+    boolean isZombie;
+
+    public Character(String name, int hp, boolean isZombie,char type){
+        this.name=name;
+        this.hp=hp;
+        this.isZombie=isZombie;
+        this.type=type;
+
+    }
+
+    
+}
