95915c3d4eb4acf15a3505427c2730c899cf2cae
Salvador <salvadorabbott11@gmail.com>
2025-06-09 15:14:13 +0200

    Excercises Functions and POO

--- Cars/Car2.java
+++ Cars/Car2.java
@@ -1,7 +1,7 @@
 package Cars;
 
 public class Car2 {
-    public static int numCars=0;
+    public static int numCar=0;
 
     int id;
     int seats;
@@ -11,25 +11,30 @@ public class Car2 {
     boolean isAuto;
     char type;
 
-    public Car2(){
-        this.brand=brand;
-        this.carDoors=carDoors;
+    public Car2 (int id,int seats,String color,int carDoors,String brand,boolean isAuto,char tyype){
+        this.id=numCar;
         this.seats=seats;
         this.color=color;
+        this.brand=brand;
+        this.carDoors=carDoors;
         this.isAuto=isAuto;
         this.type=type;
-        numCars=numCars +1;
-        this.id=numCars;
-    }
-    public static void showCar(int id,String brand, String color,String type, String carDoors,boolean isAuto,int seats){
-        System.out.println("Id:" + numCars);
-        System.out.println("Brand:"+brand);
-        System.out.println("Color:"+color);
-        System.out.println("Type:"+type);
-        System.out.println("CarDoors:"+carDoors);
-        System.out.println("¿Es Automatico?:"+isAuto);
-        System.out.println("Seats:"+seats);
+        numCar=numCar+1;
 
-        }
+
+    }
+    public void show (){
+
+        System.out.println("Id:"+ id);
+        System.out.println("Seats:"+ seats);
+        System.out.println("Color:" + color);
+        System.out.println("Brand:" + brand);
+        System.out.println("carDoors:" + carDoors);
+        System.out.println("¿Es automatico:" + isAuto);
+        System.out.println("Type:" +type);
+
+    }
+
+
     
 }
--- Cars/MainCar2.java
+++ Cars/MainCar2.java
@@ -1,13 +1,17 @@
 package Cars;
 
-import OOP.Car;
 
 public class MainCar2 {
 
     public static void main(String[] args) {
-         
-    }
+       Car2 c1=new Car2(0, 4, "Rojo", 4, "Toyota", false, 'D');
+       Car2 c2=new Car2(0,4,"Amarillo",4,"Ford",true,'F');
+       c1.show();
+       System.out.println("---------");
+       c2.show();
 
+        
     
+    }
     
 }
