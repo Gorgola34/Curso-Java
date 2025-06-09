6443d87b22a90f9049309a9d605c77cd2516c5e4
Salvador <salvadorabbott11@gmail.com>
2025-06-09 14:49:03 +0200

    Add excercises Functions

--- Funciones/Calculator.java
+++ Funciones/Calculator.java
@@ -2,74 +2,42 @@ package Funciones;
 import java.util.*;
 public class Calculator {
 
-public static int sum(int num1,int num2){
-    int result=num1+num2;
-    System.out.println("La suma es:" +result);
-    return result;
+    public static int add(int num1,int num2){
+        int result=num1+num2;
+        return result;
 
-}
-public static int subtraction (int num1,int num2){
-    int result=num1-num2;
-    System.out.println("La resta es:" + result);
-    return result;
-    
-}
-public static int multiplication(int num1,int num2){
-    int result=num1*num2;
-    System.out.println("La multiplicacion es:" + result);
-    return result;
-}
-public static int division(int num1,int num2){
-    int result=num1/num2;
-    System.out.println("La division es:" + result);
-    return result;
-    
-}
-public static int getInt(String message){
-    Scanner scanner=new Scanner(System.in);
-     System.out.println("Dime un numero entero");
-    int num=scanner.nextInt();
-    System.out.println("Dime otro numero entero");
-     //scanner.close();
-    return num;
-}
-public static void show(int result){
-    System.out.println("El resultado es:" + result);
+    }
+    public static int subtraction(int num1,int num2){
+        int result=num1-num2;
+        return result;
+    }
+    public static int multiply(int num1,int num2){
+        int result=num1*num2;
+        return result;
+        
+    }
+    public static int division(int num1,int num2){
+        int result=num1/num2;
+        return result;
+    }
 
-}
-public static void main(String[] args) {
+    public static void showOp(int result){
+        System.out.println("La suma es:" + result);
 
-     int num1=getInt("Dime un numero entero");
-     int num2=getInt("Dime otro numero entero");
-    sum(num1,num2);
-    subtraction(num1, num2);
-    multiplication(num1, num2);
-    division(num1,num2);
-    show(sum(num1,num2));
-    show(subtraction(num1, num2));
-    show(multiplication(num1, num2));
-    show(division(num1,num2));
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Introduzca numero entero");
+        int num1=scanner.nextInt();
+        System.out.println("Introduzca otro numero entero");
+        int num2=scanner.nextInt();
+        scanner.close();
+        showOp(add(num1,num2));
+        showOp(subtraction(num1, num2));
+        showOp(multiply(num1, num2));
+        showOp(division(num1, num2));
+        
+    }
    
+    
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
