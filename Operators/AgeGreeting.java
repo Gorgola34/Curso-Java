616cd1b48c56253db5b49d6909b364d7602c0fd2
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-11 10:25:21 +0200

    Add exercise If

--- Operators/AgeGreeting.java
+++ Operators/AgeGreeting.java
@@ -1,13 +1,22 @@
 package Operators;
-
+import java.util.*;
 public class AgeGreeting {
 
+    public static int getAgeByScanner(){
+        Scanner scanner=new Scanner(System.in);
+            System.out.println("Introduzca edad");
+            int age=scanner.nextInt();
+            scanner.close();
+            return age;
+    }
     public static void main(String[] args) {
+
          if(args.length==0) {
-            System.out.println("Se acabo, dime la edad");
-            return;
-        }
-        int age=Integer.valueOf(args[0]);
+        System.out.println("Se acabo, dime la edad");
+        return;
+         }
+        int age=Integer.valueOf(args[0]);  
+        
         if(age>=0&&age<18){
             System.out.println("Niño");
         }else if(age>=18&&age<33){
@@ -17,8 +26,10 @@ public class AgeGreeting {
         }else{
             System.out.println("Jubilado");
         }
+}       //int age=getAgeByScanner();                     
+}       
 
-   
-    
-}
-}
+
+
+
+
