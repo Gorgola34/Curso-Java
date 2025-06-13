cde2cc165bb79d24c6bc85b410b7a20b02ae4bb8
Salvador Abbott <salvadorabbott11@gmail.com>
2025-06-13 13:56:36 +0200

    Add excercises Bucles For

---
+++ IterationFor/Horror.java
@@ -0,0 +1,20 @@
+package IterationFor;
+
+public class Horror {
+
+    public static void main(String[] args) {
+
+        //Bucle infinito NUNCA HACER
+
+        for(int i=0;i>=0;i++){
+            System.out.println("Horror" + i);
+
+            if(i>= 100){
+                break;
+            }
+        }
+
+        
+    }
+    
+}
---
+++ IterationFor/NumberPrime.java
@@ -0,0 +1,38 @@
+package IterationFor;
+import java.util.Scanner;
+public class NumberPrime {
+
+    public static boolean isPrime(int n){
+
+            if(n<=1){
+                return false;
+        }
+
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+}
+
+public static int scannerBy(){
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("Introduce el número para ver si es Primo o no");
+    int n=scanner.nextInt();
+    scanner.close();
+    return n;
+
+}
+
+public static void main(String[] args) {
+
+    int n=scannerBy();
+    System.out.println(isPrime(n));
+}
+
+
+
+
+
+}
