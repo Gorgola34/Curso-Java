35ec392cbad72197636ae877349a333f21e5233e
Salvador <salvadorabbott11@gmail.com>
2025-06-16 19:08:56 +0200

    factorizar

---
+++ factorizar.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class factorizar {
+
+
+        public static int scannerBy(){
+            Scanner scanner=new Scanner(System.in);
+            int n=scanner.nextInt();
+            scanner.close();
+            return n;
+
+        }
+        public static int factorizar(int n){
+
+            int factorizar=1;
+            for(int i=factorizar;i<=n;i++){
+                factorizar*=i;
+            }
+            return factorizar;
+
+        }
+
+        public static void main(String[] args) {
+            int n=scannerBy();
+            System.out.println(factorizar(n));
+        }
+
+
+
+        
+    }
+
+
