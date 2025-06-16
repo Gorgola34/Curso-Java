e9c7c59bcbd304f9c96e323da6bf10038f63870e
Salvador <salvadorabbott11@gmail.com>
2025-06-16 15:02:02 +0200

    Add bucles

--- IterationWhile/ManageString.java
+++ IterationWhile/ManageString.java
@@ -2,44 +2,51 @@ package IterationWhile;
 
 public class ManageString {
 
-    public static String removeFirstLast(String object){
-        String result="";
-        for(int i=1;i>= object.length()-1;i++){
-            result= result + object.charAt(i);           
-        }    
-        return result;
-    }
+    public class ManageStrings {
 
-
-    public static void countDown(){
-            for(int i=10; i>=0; i--){
-                System.out.println(i);
-            }
-
+    static void countDown() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
         }
+    }
 
-    public static String reverse(String object){
-         String result="";
-        for(int i=object.length()-1;i>=0;i--){
-            result= result + object.charAt(i);           
-        }    
+    static String removeFirstLast(String value) {
+        String result = "";
+
+        for (int i = 1; i < value.length() - 1; i++) {
+            result = result + value.charAt(i);
+        }
         return result;
     }
 
+    static String reverseString(String value) {
+        String result = "";
 
-    public static void main(String[] args) {
-        String test="Oso baboso";
-        String remove=removeFirstLast(test);
-        String reverse=reverse(test);
-        countDown();
-        System.out.println(reverse);
-        System.out.println(remove);  
+        for (int i = 0; i < value.length(); i++) {
+            result = value.charAt(i) + result;
+        }
+        return result;
     }
 
+    static String reverseString2(String value) {
+        String result = "";
+
+        for (int i = value.length() - 1; i >= 0; i--) {
+            result = result + value.charAt(i);
+        }
+        return result;
+    }
+
+    static String removeSpaces(String text) {
+        String result = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                continue;
+            }
+            result += text.charAt(i);
+        }
+        return result;
+    }
 }
-
     
-
-    
-    
-
+}
---
+++ IterationWhile/Spaces.java
@@ -0,0 +1,24 @@
+package IterationWhile;
+
+public class Spaces {
+
+    public static String spaces(String name){
+        String result="";
+        for(int i=0;i<name.length();i++){
+            if(name.charAt(i)==' '){
+                continue;
+            }
+                
+            result +=name.charAt(i);
+            }
+            return result;
+
+        }
+       
+    public static void main(String[] args) {
+        String remove=spaces("Oso baboso");
+        System.out.println(remove);
+        
+    }
+    
+}
