87506568047e812067d9f018d4bc3f91f7d61c1c
Salvador <salvadorabbott11@gmail.com>
2025-06-21 19:50:03 +0200

    Add Piedra,papel y tijeras

--- Demos/Exercises/Game.java
+++ Demos/Exercises/Game.java
@@ -2,39 +2,54 @@ package Demos.Exercises;
 import java.util.*;
 public class Game {
 
-    public static void main(String[] args) {
-        boolean want=true;
+    public static int scannerBy(){
         Scanner scanner= new Scanner(System.in);
-        Random random = new Random();
+        int option=scanner.nextInt();
+        return option;
+    }
+    public static int random(){
+        Random random= new Random();
+        int computer=random.nextInt(3);
+        return computer;
+    }
+    public static String scanner(){
+         Scanner scanner2= new Scanner(System.in);
+        String answer=scanner2.nextLine();
+        return answer;
+    }
+       
+    public static void main(String[] args) {
 
+        boolean want=true;
         while(want){
-            String [] options={"Piedra", "Papel", "Tijeras"};
-            System.out.println("0=Piedra, 1=Papel, 2= Tijeras");
-            int option=scanner.nextInt();
-            scanner.nextLine();
-            int computer=random.nextInt(3);
-            System.out.println("Elegiste " + options[option]);
-            System.out.println("La computadora eligió " + options[computer]);
+            String[] options ={"Piedra","Papel","Tijeras"};
+            System.out.println("0=Piedra,1=Papel,2=Tijeras");
+            int option=scannerBy();
+            int computer=random();
+            System.out.println("Elegiste:  " + options[option]);
+            System.out.println("La computadora eligió: " + options[computer]);
 
             if(option==computer){
                 System.out.println("Empate");
             }else if
             ((option==0&&computer==2)||
-            (option==1&&computer==0)||
-            (option==2&&computer==1)){
+            option==1&&computer==0||
+            option==2&&computer==1){
                 System.out.println("Ganaste");
+
             }else{
                 System.out.println("Perdiste");
             }
-
-            System.out.println("¿Quieres continuar?");
-            String answer=scanner.nextLine();
-            if(answer.equals("n")||(answer.equals("no"))){
+            System.out.println("¿Quieres seguir jugando?");
+            String answer=scanner();
+            if(answer.equals("no")||
+            answer.equals("n")){
                 want=false;
             }
-            }
-            System.out.println("Gracias por jugar");
+        
         }
+        System.out.println("Gracias por jugar");
+    
     }
     
-
+}
