9473c9065ad9963abac2ee5a4c0dc5a01e0f9449
Salvador <salvadorabbott11@gmail.com>
2025-06-23 16:05:59 +0200

    OOP

---
+++ DemoOPP/src/OOP/Square.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class Square {
+    int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return 
+        super.toString() + "\n" + "Square {side: %s}".formatted(side);
+    }
+
+}
+    
+
