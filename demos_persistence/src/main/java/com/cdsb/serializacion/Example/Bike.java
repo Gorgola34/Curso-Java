29900bd89ddaf6a9907b3d75683a33f6420ad148
Salvador <salvadorabbott11@gmail.com>
2025-07-17 15:22:01 +0200

    add

--- demos_persistence/src/main/java/com/cdsb/serializacion/Example/Bike.java
+++ demos_persistence/src/main/java/com/cdsb/serializacion/Example/Bike.java
@@ -1,20 +1,18 @@
 package com.cdsb.serializacion.Example;
-public class Bike {
 
+public class Bike {
     public String color;
-    public String model;
     public int gears;
     public boolean isCustomizable;
 
     public Bike(){
 
     }
-
-    public Bike(String color, String model, int gears,boolean isCustomizable){
+    public Bike(String color, int gears, boolean isCustomizable){
         this.color=color;
-        this.model=model;
         this.gears=gears;
         this.isCustomizable=isCustomizable;
+
     }
 
 }
